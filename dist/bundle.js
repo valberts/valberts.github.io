a1b3af5bef23042f7f5101a96d901038317c0905
asakusa <albertsson.vincent@gmail.com>
2021-02-18 22:44:51 +0100

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,4 @@
+const App = () => React.createElement("div", null, "Hello React App from scratch");
+/// <reference path="App.tsx" />
+ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
+//# sourceMappingURL=bundle.js.map
